b4c76475f62fba4c225310652f15f61fd570d4e9
Анастасия <httphxq@gmail.com>
2026-02-18 15:44:10 +0300

    store.js as a database

---
+++ js/store/store.js
@@ -0,0 +1,26 @@
+const state = {
+    counties: [
+        {
+            id:1,
+            name: 'Italy',
+            status: 'Dream',
+            budget: 1500,
+            notes: 'Rome, Florence, pasta and wine'
+        },
+        {
+            id:2,
+            name: 'Spain',
+            status: 'Planned',
+            budget: 1200,
+            notes: 'Barcelona, Madrid, tapas and wine'
+        },
+        {
+            id:3,
+            name: 'France',
+            status: 'Visited',
+            budget: 1800,
+            notes: 'Paris, Lyon, cheese and wine'
+        }
+
+    ]
+}; 
